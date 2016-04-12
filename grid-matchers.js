26057dd346204ceebd85b34bb5d2d28db2c03d13
Sterling Camden <sterling@relateiq.com>
2016-04-12 16:07:48 -0700

    add to jasmine matchers

--- grid-matchers.js
+++ grid-matchers.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
 
     var tools = require('jasmine-tools-riq');
 
     function addFieldMatcher(matchers, fieldName) {
-        matchers[fieldName + 'ToBe'] = function () {
+        matchers[fieldName + 'ToBe'] = function() {
             return {
-                compare: function (actual, val) {
+                compare: function(actual, val) {
                     var actualVal = actual[fieldName];
                     var pass = actualVal === val;
                     return {
@@ -23,11 +23,21 @@
 
 
     function makeFakeRange(t, l, h, w) {
-        return {top: t, left: l, height: h, width: w};
+        return {
+            top: t,
+            left: l,
+            height: h,
+            width: w
+        };
     }
 
     function makeFakePosRange(t, l, r, b) {
-        return {top: t, left: l, right: r, bottom: b};
+        return {
+            top: t,
+            left: l,
+            right: r,
+            bottom: b
+        };
     }
 
     function maybeAddPx(v) {
@@ -36,20 +46,20 @@
 
     var matchers = {
 
-        toHaveBeenCalledWithAll: function () {
+        toHaveBeenCalledWithAll: function() {
             return {
-                compare: function (actual, argsArrays) {
+                compare: function(actual, argsArrays) {
                     var spy = actual;
 
                     var numCalls = spy.calls.count() === argsArrays.length;
                     var allArrgs = true;
-                    argsArrays.forEach(function (args, index) {
+                    argsArrays.forEach(function(args, index) {
                         var argsForCall = spy.calls.argsFor(index);
                         if (!argsForCall) {
                             allArrgs = false;
                         } else {
                             if (angular.isArray(args)) {
-                                args.forEach(function (arg, index) {
+                                args.forEach(function(arg, index) {
                                     if (arg !== argsForCall[index]) {
                                         allArrgs = false;
                                     }
@@ -68,19 +78,19 @@
                 }
             };
         },
-        toBeDirty: tools.defineBasicMatcher(function (actual) {
+        toBeDirty: tools.defineBasicMatcher(function(actual) {
             return actual.isDirty();
-        }, function (actual, expected, pass) {
+        }, function(actual, expected, pass) {
             return tools.expectedObjectWithNot(actual, pass) + ' to be dirty';
         }),
-        toBeClean: tools.defineBasicMatcher(function (actual) {
+        toBeClean: tools.defineBasicMatcher(function(actual) {
             return actual.isClean();
-        }, function (actual, exp, pass) {
+        }, function(actual, exp, pass) {
             return tools.expectedObjectWithNot(actual, pass) + ' to be clean';
         }),
-        rangeToBe: function () {
+        rangeToBe: function() {
             return {
-                compare: function (actual, t, l, h, w) {
+                compare: function(actual, t, l, h, w) {
                     var pass = actual.top === t && actual.left === l && actual.height === h && actual.width === w;
                     return {
                         pass: pass,
@@ -89,9 +99,9 @@
                 }
             };
         },
-        toBePositioned: function () {
+        toBePositioned: function() {
             return {
-                compare: function (actual, t, l, b, r) {
+                compare: function(actual, t, l, b, r) {
                     var top = $(actual).css('top');
                     var left = $(actual).css('left');
                     var right = $(actual).css('right');
@@ -109,21 +119,44 @@
                 }
             };
         },
-        toHaveBeenBoundWith: tools.defineBasicMatcher(function (actual, name, elem) {
+        toHaveBeenBoundWith: tools.defineBasicMatcher(function(actual, name, elem) {
             var spy = actual;
             var hasName = spy.calls.argsFor(0)[0] === name;
             var hasElem = !elem || spy.calls.argsFor(0)[1] === elem;
             var isFunction = hasElem && angular.isFunction(spy.calls.argsFor(0)[2]) || angular.isFunction(spy.calls.argsFor(0)[1]);
             return hasName && hasElem && isFunction;
-        })
+        }),
+        toHaveBeenCalledWithAllPointsInRange: function() {
+            return {
+                compare: function(spy, range) {
+                    var allArgs = spy.calls.allArgs();
+                    var fails = [];
+                    for (var r = range.top; r < range.top + range.height; r++) {
+                        for (var c = range.left; c < range.left + range.width; c++) {
+                            var hadArgs = allArgs.some(function(args) {
+                                return args[0] === r && args[1] === c;
+                            });
+                            if (!hadArgs) {
+                                fails.push(r + ',' + c);
+                            }
+                        }
+                    }
+                    var pass = !fails.length;
+                    return {
+                        pass: pass,
+                        message: 'Expected ' + spy.and.identity() + (!pass ? '' : ' not') + ' to have been called with all points in range ' + JSON.stringify(range) + ' but ' + JSON.stringify(fails) + ' were missing'
+                    };
+                }
+            };
+        }
     };
 
     var commonFields = ['row', 'col', 'top', 'left', 'width', 'height', 'units', 'space', 'class', 'gridX', 'gridY'];
-    commonFields.forEach(function (fieldName) {
+    commonFields.forEach(function(fieldName) {
         addFieldMatcher(matchers, fieldName);
     });
 
-    beforeEach(function () {
+    beforeEach(function() {
         jasmine.addMatchers(matchers);
     });
 })
